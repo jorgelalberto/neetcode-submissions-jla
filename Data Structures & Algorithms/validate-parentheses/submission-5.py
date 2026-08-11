841d58732343939d5985823cc61c96ae79f25923
Jorge Luis Alberto <92881097+jorgelalberto@users.noreply.github.com>
2026-08-11 16:02:14 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_to_close = {'(':')','{':'}', '[':']'}
+        close_to_open = {')':'(', '}':'{', ']':'['}
+        stacky = []
+
+        for char in s:
+            if char in open_to_close:
+                stacky.append(char)
+            elif char in close_to_open and stacky and stacky[-1] == close_to_open[char]:
+                stacky.pop()
+            elif char in close_to_open and ((stacky and stacky[-1] != close_to_open[char]) or not stacky):
+                return False
+
+        return False if stacky else True 
