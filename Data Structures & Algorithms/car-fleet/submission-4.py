016f227d0da4e330ab90e0850e73b064320b6814
Jorge Luis Alberto <92881097+jorgelalberto@users.noreply.github.com>
2026-08-12 08:01:58 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        targets = [None]*target
+        fleets = 0
+        prev_fleet_arrival = 0
+
+        for position_i, speed_i in zip(position, speed):
+            targets[position_i] = speed_i
+
+        for i, speed_i in enumerate(reversed(targets)):
+            if speed_i == None:
+                continue
+
+            position_i = len(targets)-1 - i
+
+            curr_fleet_arrival = (target-position_i)/speed_i
+            # same fleet
+            if curr_fleet_arrival <= prev_fleet_arrival:
+                continue
+            # different fleet
+            prev_fleet_arrival = max(prev_fleet_arrival, curr_fleet_arrival)
+            fleets += 1
+        return fleets
